cd27158b987a115eadf2e5fd2f671ef9b28409e5
Samuel Lucidi <slucidi@newstex.com>
2014-03-26 00:30:03 -0400

    Get downloads working.

--- cellardoor/server.py
+++ cellardoor/server.py
@@ -1,6 +1,7 @@
 from cgi import parse_qs, escape
 import re
 import json
+import mimetypes
 
 def not_found(environ, start_response):
     """
@@ -11,14 +12,74 @@ def not_found(environ, start_response):
     start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
     return ['Not Found']
 
-def package_index(environ, start_response, parameters):
+def pkg_index(environ, start_response, parameters):
     """
     Return the local package index.
 
     """
 
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['package_index']
+    from cache import list_packages
+
+    response = "<html>"
+    response += "\t<head>"
+    response += "\t\t<title>Package Index</title>"
+    response += "\t</head>"
+    response += "\t<body>"
+
+    pkgs = list_packages()
+    for pkg in pkgs:
+        response += "<a href='/packages/%s/'>%s</a><br>" % (pkg, pkg)
+    response += "\t</body>"
+    response += "</html>"
+
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [response]
+
+def pkg_files(environ, start_response, parameters):
+    """
+    Return the list of distributions for the selected package.
+
+    """
+
+    from cache import list_distributions
+
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    pkg = path.split('/')[1]
+
+    response = "<html>"
+    response += "\t<head>"
+    response += "\t\t<title>%s</title>" % (pkg)
+    response += "\t</head>"
+    response += "\t<body>"
+
+    dists = list_distributions(pkg)
+    if dists == []:
+        return not_found(environ, start_response)
+
+    for dist in dists:
+        response += "<a href='/packages/%s/%s'>%s</a><br>" % (pkg, dist, dist)
+    response += "\t</body>"
+    response += "</html>"
+
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [response]
+
+def download_dist(environ, start_response, parameters):
+    """
+
+    """
+
+    from cache import get_distribution
+
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    pkg = path.split('/')[1]
+    dist_name = path.split('/')[2]
+    mimetype = mimetypes.guess_type(path, strict=True)
+
+    dist = get_distribution(pkg, dist_name)
+
+    start_response('200 OK', [('Content-Type', mimetype[0])])
+    return dist
 
 def index(environ, start_response, parameters):
     """
@@ -38,8 +99,9 @@ def router(environ, start_response):
 
     routes = [
     (r'^$', index),
-    (r'packages/?$', package_index),
-    (r'packages/(.+)$', package_index)
+    (r'packages/?$', pkg_index),
+    (r'packages/([^\/]+)/$', pkg_files),
+    (r'packages/([^/]+)/([^/]+)$', download_dist)
     ]
 
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
